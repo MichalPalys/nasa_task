8492fa3d4255f180af683344d78a3aa9493a0913
Michał Pałys <michal.palys@jsql.pl>
2019-07-02 11:16:10 +0200

    inject ChristmasDaysService into NasaPhotoService

--- src/Service/NasaPhotoService.php
+++ src/Service/NasaPhotoService.php
@@ -4,5 +4,15 @@ namespace App\Service;
 
 class NasaPhotoService
 {
+    private $christmasDaysService;
 
+    public function __construct(ChristmasDaysService $christmasDaysService)
+    {
+        $this->christmasDaysService = $christmasDaysService;
+    }
+
+    public function setNasaImageRelatedWithPolishHolidaysToDatabaseByYear(int $year):void
+    {
+        $holidaysArrayByYear = $this->getPolishHolidaysArrayByYear($year);
+    }
 }
