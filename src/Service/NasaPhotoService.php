14f9dd9ed933634827e0111e88d20d625803e856
Michał Pałys <michal.palys@jsql.pl>
2019-07-02 13:03:45 +0200

    inject httpClient into NasaPhotoService

--- src/Service/NasaPhotoService.php
+++ src/Service/NasaPhotoService.php
@@ -3,16 +3,23 @@
 namespace App\Service;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class NasaPhotoService
 {
     private $christmasDaysService;
     private $params;
+    private $httpClient;
 
-    public function __construct(ChristmasDaysService $christmasDaysService, ParameterBagInterface $params)
+    public function __construct(
+        ChristmasDaysService $christmasDaysService,
+        ParameterBagInterface $params,
+        HttpClientInterface $httpClient
+    )
     {
         $this->christmasDaysService = $christmasDaysService;
         $this->params = $params;
+        $this->httpClient = $httpClient;
     }
 
     public function setNasaImageRelatedWithPolishHolidaysToDatabaseByYear(int $year):void
